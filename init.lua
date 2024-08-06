c6c91245d8a54690dddf7dcde3a442c74be2c3c3
Sascha Loishandl <sascha.loishandl@gmail.com>
2024-08-06 11:53:12 +0200

    added typescript and html lsp

--- init.lua
+++ init.lua
@@ -729,6 +729,8 @@ require('lazy').setup({
         omnisharp = {},
         phpactor = {},
         tailwindcss = {},
+        tsserver = {},
+        html = {},
         -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
         --
         -- Some languages (like typescript) have entire language plugins that can be useful:
