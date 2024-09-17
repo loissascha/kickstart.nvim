8f9e81bf550d3730565b44d2895ccdd881be9481
Sascha Loishandl <sascha.loishandl@gmail.com>
2024-09-17 17:50:28 +0200

    update

--- lua/custom/plugins/colorscheme.lua
+++ lua/custom/plugins/colorscheme.lua
@@ -23,7 +23,22 @@ return {
     priority = 1000,
     opts = {},
     config = function()
-      require('github-theme').setup {}
+      local palettes = {
+        github_dark_default = {
+          -- bg0 = '#000000',
+          -- bg1 = '#000000',
+          -- bg2 = '#000000',
+          -- bg3 = '#000000',
+          -- bg4 = '#000000',
+          -- fg0 = '#000000',
+          -- fg1 = '#000000',
+          -- fg2 = '#000000',
+          -- fg3 = '#000000',
+          sel0 = '#ada',
+          sel1 = '#ada',
+        },
+      }
+      require('github-theme').setup { palettes = palettes }
       vim.cmd 'colorscheme github_dark_default'
     end,
   },
@@ -45,75 +60,75 @@ return {
   --     -- vim.cmd.colorscheme 'solarized-osaka'
   --   end,
   -- },
-  {
-    'slugbyte/lackluster.nvim',
-    lazy = false,
-    priority = 1000,
-    init = function()
-      -- vim.cmd.colorscheme 'lackluster'
-      -- vim.cmd.colorscheme("lackluster-hack") -- my favorite
-      -- vim.cmd.colorscheme("lackluster-mint")
-    end,
-  },
-  {
-    'bluz71/vim-moonfly-colors',
-    name = 'moonfly',
-    lazy = false,
-    priority = 1000,
-  },
-  {
-    'rose-pine/neovim',
-    name = 'rose-pine',
-    init = function()
-      -- vim.cmd.colorscheme 'rose-pine'
-    end,
-    config = function()
-      local rosepine = require 'rose-pine'
-      rosepine.setup {
-        palette = {
-          -- moon = {
-          --   base = '#161719',
-          -- },
-        },
-      }
-    end,
-  },
+  -- {
+  --   'slugbyte/lackluster.nvim',
+  --   lazy = false,
+  --   priority = 1000,
+  --   init = function()
+  --     -- vim.cmd.colorscheme 'lackluster'
+  --     -- vim.cmd.colorscheme("lackluster-hack") -- my favorite
+  --     -- vim.cmd.colorscheme("lackluster-mint")
+  --   end,
+  -- },
+  -- {
+  --   'bluz71/vim-moonfly-colors',
+  --   name = 'moonfly',
+  --   lazy = false,
+  --   priority = 1000,
+  -- },
+  -- {
+  --   'rose-pine/neovim',
+  --   name = 'rose-pine',
+  --   init = function()
+  --     -- vim.cmd.colorscheme 'rose-pine'
+  --   end,
+  --   config = function()
+  --     local rosepine = require 'rose-pine'
+  --     rosepine.setup {
+  --       palette = {
+  --         -- moon = {
+  --         --   base = '#161719',
+  --         -- },
+  --       },
+  --     }
+  --   end,
+  -- },
 
-  {
-    'catppuccin/nvim',
-    name = 'catppuccin',
-    priority = 1000,
-    opts = {
-      -- transparent_background = true,
-    },
-    init = function()
-      -- Load the colorscheme here.
-      -- vim.cmd.colorscheme 'catppuccin'
-    end,
-    config = function()
-      local catp = require 'catppuccin'
-      catp.setup {
-        color_overrides = {
-          mocha = {
-            base = '#161719',
-            mantle = '#161719',
-            crust = '#161719',
-            -- base = '#000000',
-            -- mantle = '#000000',
-            -- crust = '#000000',
-          },
-        },
-      }
-      if vim.g.neovide then
-        catp.setup {
-          transparent_background = true,
-        }
-      else
-        -- make background transparent when not in neovide for transparency in terminal
-        -- catp.setup {
-        --   transparent_background = true,
-        -- }
-      end
-    end,
-  },
+  -- {
+  --   'catppuccin/nvim',
+  --   name = 'catppuccin',
+  --   priority = 1000,
+  --   opts = {
+  --     -- transparent_background = true,
+  --   },
+  --   init = function()
+  --     -- Load the colorscheme here.
+  --     -- vim.cmd.colorscheme 'catppuccin'
+  --   end,
+  --   config = function()
+  --     local catp = require 'catppuccin'
+  --     catp.setup {
+  --       color_overrides = {
+  --         mocha = {
+  --           base = '#161719',
+  --           mantle = '#161719',
+  --           crust = '#161719',
+  --           -- base = '#000000',
+  --           -- mantle = '#000000',
+  --           -- crust = '#000000',
+  --         },
+  --       },
+  --     }
+  --     if vim.g.neovide then
+  --       catp.setup {
+  --         transparent_background = true,
+  --       }
+  --     else
+  --       -- make background transparent when not in neovide for transparency in terminal
+  --       -- catp.setup {
+  --       --   transparent_background = true,
+  --       -- }
+  --     end
+  --   end,
+  -- },
 }
